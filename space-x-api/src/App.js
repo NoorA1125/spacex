d36af67e1e814f79a44fd9e5d01be97d82ee90a5
Noor Abdikadir <nooromar305@gmail.com>
2021-07-09 15:08:57 -0700

    got rockets to populate. Time to style the grid

--- space-x-api/src/App.js
+++ space-x-api/src/App.js
@@ -7,56 +7,53 @@ import Hero from "./components/Hero";
 import 'bulma/css/bulma.min.css';
 import './styles.css';
 
-const useFetch = url => { //API call
-  const [rocket, setRocket] = useState(null);
-  const [loading, setLoading] = useState(true);
+class App extends Component {
+  constructor() {
+    super();
 
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(async () => {
-    const response = await fetch(url);
-    const rocket = await response.json();
-    const [item] = rocket;
-    setRocket(item);
-    setLoading(false);
-  }, []);
+    this.state = {
+      rockets: [] //our rockets will be retrieved by the API call. Thats why its an empty array.
+    }
+  }
 
-  return { rocket, loading };
-};
+  componentDidMount() { //API call using SpaceX API
+    fetch("https://api.spacexdata.com/v4/rockets")
+      .then(res => res.json())
+      .then(rockets => this.setState({ rockets: rockets }))
+  }
 
-function App() {
-  const [count, setCount] = useState(0);
-  const { rocket, loading } = useFetch("https://api.spacexdata.com/v4/rockets");
-  console.log(rocket) //Seeing is API calls went through
-
-  return (
-    <>
-    <BulmaNavbar />
-    <Hero />
-    <br />
-    {/* Code for Cards to be generated */}
-    <div className="columns features"> 
-            <div className="column is-4">
-                <div className="card is-shady">
-                    <div className="card-image">
-                        <figure className="image is-4by3">
-                            <img src={rocket.flickr_images[0]} alt={rocket.name} />
-                        </figure>
-                    </div>
-                    <div className="card-content">
-                        <div className="content">
-                            <h4>Tempor orci dapibus faber in.</h4>
-                            <p>Ut venenatis tellus in metus vulputate. Amet consectetur adipiscing elit pellentesque. Sed arcu non odio euismod lacinia at quis risus. Faucibus turpis in eu mi bibendum neque egestas cmonsu songue. Phasellus vestibulum lorem
-                sed risus.</p>
-                            <span className="button is-link modal-button" data-target="modal-card">Modal Card</span>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <>
+        <BulmaNavbar />
+        <Hero />
+        <br />
+        {/* Code for Cards to be generated */}
+        {this.state.rockets.map((rocket) => (
+        <div className="columns features">
+          <div className="column is-4">
+            <div className="card is-shady">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src="" alt="Space X API Rockets" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="content">
+                  <h4>{rocket.name}</h4>
+                  <p>{rocket.description}</p>
+                  <span className="button is-link modal-button" data-target="modal-card"><a href={rocket.wikipedia} target="_blank">Learn More</a></span>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-
+        ))}
         {/* Code for Modal to pop up */}
-    </>
-  );
+
+      </>
+    );
+  }
 }
 
 export default App;
