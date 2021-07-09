99361deae3e6e0728bd3faa75d5f28ce3118c12f
Noor Abdikadir <nooromar305@gmail.com>
2021-07-09 14:12:41 -0700

    setting up file. Removed static codes. Imported files I will create/components for the reuseability of the site/codes

--- space-x-api/src/App.js
+++ space-x-api/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Component } from 'react';
+import BulmaNavbar from "./components/Navbar";
+import BulmaCard from "./components/Card";
+import BulmaModal from "./components/Modal";
+import Hero from "./components/Hero";
+import 'bulma/css/bulma.min.css';
+import './styles.css';
+
+const useFetch = url => { //API call
+  const [rocket, setRocket] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(async () => {
+    const response = await fetch(url);
+    const rocket = await response.json();
+    const [item] = rocket;
+    setRocket(item);
+    setLoading(false);
+  }, []);
+
+  return { rocket, loading };
+};
 
 function App() {
+  const [count, setCount] = useState(0);
+  const { rocket, loading } = useFetch("https://api.spacexdata.com/v4/rockets");
+  console.log(rocket) //Seeing is API calls went through
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    </>
   );
 }
 
